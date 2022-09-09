fb48c139e5d6a34bd902b84ba682d1bc7b7a85dd
prithivi5667 <prithivi5667@gmail.com>
2022-09-09 18:21:01 +0530

    g

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express=require("express")
 require('dotenv').config()
 const app=express()
 const cors=require("cors")
-app.use(cors())
 app.use(express.json())
 require('./db/config')
+app.use(cors())
+
 const userRouter=require("./routes/Auth")
 app.use('/api',userRouter)
 const PORT=5000 ||process.env.PORT
